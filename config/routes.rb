6aca5d41d33d4156e0a871ca8f638670388de7c4
AnaBondiguel <michaelyangyang@hotmail.com>
2022-06-07 22:55:25 +1000

    implement authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :listings
-  devise_for :users
   root 'pages#home'
+  devise_for :users
+  resources :listings
+  
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
